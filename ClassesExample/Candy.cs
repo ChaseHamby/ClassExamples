05baa737ae9eb12c1ff6e0472d2c21bf2b82f9f6
unknown <c.chase.hamby@gmail.com>
2019-03-02 13:31:30 -0600

    class examples

---
+++ ClassesExample/Candy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExample
+{
+    class Candy
+    {
+        // Properties
+        public string Flavor { get; } // If property is public - the getter/setter are public unless specified
+        public int Rating { get; private set; }
+        public string Review { get; private set; }
+        public string Name { get; } 
+        public CandyType Type { get; }
+
+
+        // Fields
+        const int MinRatingValue = 1; // const fields must be upper case // implicitly private
+        const int MaxRatingValue = 10;
+
+        // Constructor
+        public Candy(string flavor, string name, CandyType type)
+        {
+            Flavor = flavor;
+            Name = name;
+            Type = type;
+        }
+        
+        // Methods
+        public void SetRating(int userRating) // accessability / return Type / name / parameter list
+        {
+            if(userRating >= MinRatingValue && userRating <= MaxRatingValue) // between 1-10
+            {
+                Rating = userRating;
+            }
+            else
+            {
+                throw new Exception($"You are dumb. Ratings are {MinRatingValue} - {MaxRatingValue}.");
+            }
+        }
+
+        public void SetRating(int userRating, string review)
+        {
+            SetRating(userRating);
+            Review = review;
+        }
+
+        public override string ToString()
+        {
+            return $"This is {Type} {Name}, with a(n) {Flavor} flavor, rated {Rating} with a review of {Review}.";
+        }
+    }
+
+    enum CandyType // enum defined by list of options that can be stored as a property
+    {
+        Chocolate,
+        HardCandy,
+        Caramel,
+        Sour,
+        Stretchy,
+        Nut
+    }
+}
---
+++ ClassesExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassesExample // Namespace // Used just like a component in React
+{
+    class Program // Class
+    {
+        //private string _myStuff = "for me"; 
+
+        //string _myData = "this is just for me to use"; // Field // convention is to underscore then camelCase
+
+        //public int Number { get; set; } // Property // you can set the value of this property and get the value // 'autoproperty'
+
+        //public void DoTheThing()
+        //{
+        //    Number = 1;
+        //    var x = Number;
+        //}
+
+        static void Main(string[] args)
+        {
+            var taffy = new Candy("Orange", "Orange Saltwater Taffy", CandyType.Stretchy);
+
+            taffy.SetRating(10);
+            taffy.SetRating(5, "I kinda liked it.");
+
+            Console.WriteLine(taffy);
+
+            Console.WriteLine("Hello World!"); // Method // Used to take an action
+        }
+    }
+}
